4d4275e04cd4ebc491c5bc2caca5924afd15b187
saadghazal <saad.m.ghazal@icloud.com>
2023-11-25 17:27:58 +0300

    Creating Label With Custom Color using darker and brighter

--- src/JFrame1.java
+++ src/JFrame1.java
@@ -12,10 +12,23 @@ public class JFrame1 extends JFrame {
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container contentPane = getContentPane();
+        contentPane.setLayout(new GridLayout(5,3));
         JLabel label = new JLabel("Label"); // creating label
         Font yaserFont = new Font("SanSerif",Font.ITALIC,40); // creating font
+        Color originalColor = new Color(95, 189, 255); // creating the color with red , green ,blue
+        label.setForeground(originalColor); // setting the original color to label
         label.setFont(yaserFont); // setting the created font to the label
         contentPane.add(label);//adding the label to the content pane
+        JLabel label1 = new JLabel("Label1"); // creating label 1
+        label1.setFont(yaserFont); // setting font to label 1
+        label1.setForeground(originalColor.brighter()); // set the color that is brighter than the original color
+        contentPane.add(label1);
+        JLabel label2 = new JLabel("Label2"); // creating label
+        label2.setFont(yaserFont); // setting font to label 2
+        label2.setForeground(originalColor.darker());// set the color that is darker than the original color
+        contentPane.add(label2);
+
+
         setContentPane(contentPane);
 
     }
