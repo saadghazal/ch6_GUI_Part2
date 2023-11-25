5cbb4c414df02d9cee4255e99748e7c46c2bbced
saadghazal <saad.m.ghazal@icloud.com>
2023-11-25 17:15:24 +0300

    Creating Label With Custom Font

--- src/JFrame1.java
+++ src/JFrame1.java
@@ -12,9 +12,10 @@ public class JFrame1 extends JFrame {
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container contentPane = getContentPane();
-
-
-
+        JLabel label = new JLabel("Label"); // creating label
+        Font yaserFont = new Font("SanSerif",Font.ITALIC,40); // creating font
+        label.setFont(yaserFont); // setting the created font to the label
+        contentPane.add(label);//adding the label to the content pane
         setContentPane(contentPane);
 
     }
